8aa42cd605b2d1946c7cb31ae140dec1f727829e
Felix Krull <113671193+felixkrull-neuland@users.noreply.github.com>
2024-03-19 15:31:32 +0100

    feedproducer: add datareplication dependency

--- feedproducer/build.gradle.kts
+++ feedproducer/build.gradle.kts
@@ -7,4 +7,16 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven {
+        name = "GitHubPackages"
+        url = uri("https://maven.pkg.github.com/datareplication/datareplication-java")
+        credentials {
+            username = project.findProperty("datareplication.username") as String?
+            password = findProperty("datareplication.password") as String?
+        }
+    }
+}
+
+dependencies {
+    implementation("io.datareplication:datareplication:0.0.1-SNAPSHOT")
 }
